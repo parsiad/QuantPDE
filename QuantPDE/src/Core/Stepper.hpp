af652f452d61e1c4818f949bf11e9bd6dcd95185
parsiad <parsiad.azimzadeh@gmail.com>
2014-06-19 18:41:49 -0700

    Removed all branches from steppers using template specialization.

--- QuantPDE/src/Core/Stepper.hpp
+++ QuantPDE/src/Core/Stepper.hpp
@@ -6,7 +6,7 @@ namespace QuantPDE {
 /**
  * Steps through time at constant intervals.
  */
-template <bool Forward>
+template <bool>
 class ConstantStepper final : public Iteration {
 
 	Real startTime, endTime, dt;
@@ -16,14 +16,9 @@ class ConstantStepper final : public Iteration {
 		n = 0;
 	}
 
-	virtual Real initialTime(Real) const {
-		return Forward ? startTime : endTime; // TODO: Optimize
-	}
+	virtual Real initialTime(Real) const;
 
-	virtual Real timestep() {
-		n++;
-		return Forward ? dt : -dt; // TODO: Optimize
-	}
+	virtual Real timestep();
 
 	virtual bool done() const {
 		return n >= steps;
@@ -60,23 +55,40 @@ public:
 typedef ConstantStepper<false> ReverseConstantStepper;
 typedef ConstantStepper<true > ForwardConstantStepper;
 
+template <>
+Real ReverseConstantStepper::initialTime(Real) const {
+	return endTime;
+}
+
+template <>
+Real ForwardConstantStepper::initialTime(Real) const {
+	return startTime;
+}
+
+template <>
+Real ReverseConstantStepper::timestep() {
+	n++;
+	return -dt;
+}
+
+template <>
+Real ForwardConstantStepper::timestep() {
+	n++;
+	return dt;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
  * Steps through time at (self-adjusting) variable intervals.
  */
-template <bool Forward>
+template <bool>
 class VariableStepper final : public Iteration {
 
 	Real startTime, endTime, dt, target, scale, time;
-	Real (VariableStepper<Forward>::*_step)();
+	Real (VariableStepper::*_step)();
 
-	Real _step0() {
-		_step = &VariableStepper::_step1;
-		return Forward ? dt : -dt; // TODO: Optimize
-	}
-
-	Real _step1() {
+	inline void __step1() {
 		const Vector
 			&v1 = iterand(0),
 			&v0 = iterand(1)
@@ -90,49 +102,24 @@ class VariableStepper final : public Iteration {
 				)
 			)
 		).maxCoeff();
-
-		// TODO: Optimize
-		if(Forward) {
-			if(time + dt > endTime) {
-				dt = endTime - time;
-				time = endTime;
-			} else {
-				time += dt;
-			}
-
-			return dt;
-		} else {
-			if(time - dt < startTime) {
-				dt = time - startTime;
-				time = startTime;
-			} else {
-				time -= dt;
-			}
-
-			return -dt;
-		}
 	}
+
+	Real _step0();
+
+	Real _step1();
 
 	virtual void clear() {
 		_step = &VariableStepper::_step0;
-		time = Forward ? startTime : endTime; // TODO: Optimize
+		time = initialTime(0.);
 	}
 
-	virtual Real initialTime(Real) const {
-		return endTime;
-	}
+	virtual Real initialTime(Real) const;
 
 	virtual Real timestep() {
 		return (this->*_step)();
 	}
 
-	virtual bool done() const {
-		if(Forward) {
-			return time <= startTime;
-		} else {
-			return time >= endTime;
-		}
-	}
+	virtual bool done() const;
 
 public:
 
@@ -173,6 +160,66 @@ public:
 
 typedef VariableStepper<false> ReverseVariableStepper;
 typedef VariableStepper<true > ForwardVariableStepper;
+
+template <>
+Real ReverseVariableStepper::_step1() {
+	__step1();
+
+	if(time - dt < startTime) {
+		dt = time - startTime;
+		time = startTime;
+	} else {
+		time -= dt;
+	}
+
+	return -dt;
+}
+
+template <>
+Real ForwardVariableStepper::_step1() {
+	__step1();
+
+	if(time + dt > endTime) {
+		dt = endTime - time;
+		time = endTime;
+	} else {
+		time += dt;
+	}
+
+	return dt;
+}
+
+template <>
+Real ReverseVariableStepper::_step0() {
+	_step = &VariableStepper::_step1;
+	return -dt;
+}
+
+template <>
+Real ForwardVariableStepper::_step0() {
+	_step = &VariableStepper::_step1;
+	return dt;
+}
+
+template <>
+Real ReverseVariableStepper::initialTime(Real) const {
+	return endTime;
+}
+
+template <>
+Real ForwardVariableStepper::initialTime(Real) const {
+	return startTime;
+}
+
+template <>
+bool ReverseVariableStepper::done() const {
+	return time <= startTime;
+}
+
+template <>
+bool ForwardVariableStepper::done() const {
+	return time >= endTime;
+}
 
 } // QuantPDE
 
