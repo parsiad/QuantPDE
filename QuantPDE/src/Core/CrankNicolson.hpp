c9d1af6bba7c30970fc6813855b16ffe658b8430
parsiad <parsiad.azimzadeh@gmail.com>
2015-01-30 19:08:00 -0500

    Added explicit method.

--- QuantPDE/src/Core/CrankNicolson.hpp
+++ QuantPDE/src/Core/CrankNicolson.hpp
@@ -1,6 +1,8 @@
 #ifndef QUANT_PDE_CORE_CRANK_NICOLSON_HPP
 #define QUANT_PDE_CORE_CRANK_NICOLSON_HPP
 
+#include <climits>
+
 /**
  * QuantPDE namespace.
  */
@@ -15,10 +17,16 @@ namespace QuantPDE {
  * t/2\right]\mathbf{x}^0 + \left[b\left(t^1\right) + b\left(t^0\right)\right]/2
  * \f]
  *
+ * @tparam ThetaInverse 1 for implicit method, 2 for Crank-Nicolson, and
+ *                      std::numeric_limits<Real>::infinity() for infinity.
+ *
  * @see QuantPDE::TimeIteration
  */
-template <Index Dimension, bool Forward>
+template <Index Dimension, bool Forward, int ThetaInverse = 2>
 class CrankNicolson : public Discretization<Dimension> {
+
+	static_assert(ThetaInverse > 0, "ThetaInverse must be positive.");
+	static constexpr Real theta = 1. / ((Real) ThetaInverse);
 
 	LinearSystem &system;
 
@@ -35,9 +43,15 @@ class CrankNicolson : public Discretization<Dimension> {
 	}
 
 	virtual Matrix Ad(Real t1) {
+		// Explicit
+		if(theta < QuantPDE::epsilon) {
+			return this->domain.identity();
+		}
+
+		// Not explicit
 		return
 			this->domain.identity()
-			+ system.A(t1) * dt() / 2.
+			+ system.A(t1) * theta * dt()
 		;
 	}
 
@@ -45,15 +59,23 @@ class CrankNicolson : public Discretization<Dimension> {
 		const Real    t0 = this->time(0);
 		const Vector &v0 = this->iterand(0);
 
+		// TODO: Optimize for explicit method
+
 		return (
 			this->domain.identity()
-			- system.A(t0) * dt() / 2.
-		) * v0 + ( system.b(t1) + system.b(t0) ) / 2.;
+			- system.A(t0) * (1-theta) * dt()
+		) * v0 + ( theta * system.b(t1) + (1-theta) * system.b(t0) );
 	}
 
 public:
 
 	virtual bool isATheSame() const {
+		// Explicit
+		if(theta < QuantPDE::epsilon) {
+			return true;
+		}
+
+		// Not explicit
 		return this->isTimestepTheSame() && system.isATheSame();
 	}
 
@@ -86,6 +108,22 @@ typedef ReverseCrankNicolson<3> ReverseCrankNicolson3;
 typedef ForwardCrankNicolson<1> ForwardCrankNicolson1;
 typedef ForwardCrankNicolson<2> ForwardCrankNicolson2;
 typedef ForwardCrankNicolson<3> ForwardCrankNicolson3;
+
+template <Index Dimension>
+using ReverseExplicitMethod = CrankNicolson<Dimension, false,
+	std::numeric_limits<int>::max()>;
+
+template <Index Dimension>
+using ForwardExplicitMethod = CrankNicolson<Dimension, true,
+	std::numeric_limits<int>::max()>;
+
+typedef ReverseExplicitMethod<1> ReverseExplicitMethod1;
+typedef ReverseExplicitMethod<2> ReverseExplicitMethod2;
+typedef ReverseExplicitMethod<3> ReverseExplicitMethod3;
+
+typedef ForwardExplicitMethod<1> ForwardExplicitMethod1;
+typedef ForwardExplicitMethod<2> ForwardExplicitMethod2;
+typedef ForwardExplicitMethod<3> ForwardExplicitMethod3;
 
 } // QuantPDE
 
