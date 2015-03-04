9719dff588b0ab90854875d51d152a681e751446
parsiad <parsiad.azimzadeh@gmail.com>
2015-03-03 20:53:31 -0500

    Fixed error in formulation of nonrecourse loan.

--- test/nonrecourse_loan.cpp
+++ test/nonrecourse_loan.cpp
@@ -23,28 +23,27 @@ using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Real S_0             = 100.;  // Initial stock value
-Real L_0             = 120.;  // Initial loan value
+Real beta_lo         = 0.8;   // Low trigger
+Real beta_hi         = 0.9;   // High trigger
+
+Real S_0             = 125.;  // Initial stock value
+Real L_0             = 100.;  // Initial loan value
 Real L_hat           = 100.;  // Representative value of L
 
 Real r               = 0.04;  // Interest rate
-Real s               = 0.;    // Spread
+Real s               = 0.02;  // Spread
 Real sigma           = 0.2;   // Volatility
 
 Real lambda          = 0.05;  // Jump arrival rate
 Real mu_xi           = -.8;   // Mean jump amplitude
 Real sigma_xi        = .42;   // Jump amplitude standard deviation
 
-Real beta_lo         = 0.7;   // Low trigger
-Real beta_hi         = 0.9;   // High trigger
-
 Real T               = 1.;    // Expiry
 
-int borrowerEvents   = 12;    // Walk-away or repay the loan
-int bankEvents       = 12;    // Request top-up or liquidate
+int events           = -1;    // Bank and borrower events (-1 for all times)
 int interestPayments = 12;    // Interest payments
 
-int N_0              = 48;    // Initial number of steps
+int N_0              = 12;    // Initial number of steps
 int maxRefinement    = 8;     // Maximum number of times to refine
 
 bool newton          = false; // TODO: Newton iteration to find optimal spread
@@ -110,6 +109,7 @@ int main(int argc, char **argv) {
 
 	// Populate vector of interest payment dates
 	{
+		interestPaymentDates.reserve(interestPayments);
 		const Real dt = T / interestPayments;
 
 		interestPaymentDates.reserve(interestPayments);
@@ -118,19 +118,23 @@ int main(int argc, char **argv) {
 		}
 	} // 2015-02-28: checked
 
-	// Hand-picked grid, scaled by S_0
+	// Hand-picked grid
+	// TODO: Make this more robust; place nodes at S_0 and L_0
 	RectilinearGrid1 grid(
 		(S_0 / 100.) * Axis {
-			0., 5., 10., 15., 20., 25.,
-			30., 35., 40., 45.,
-			50., 55., 60., 65., 70., 72.5, 75., 77.5, 80., 82., 84.,
-			86., 88., 90., 91., 92., 93., 94., 95.,
-			96., 97., 98., 99., 100.,
-			101., 102., 103., 104., 105., 106.,
-			107., 108., 109., 110., 112., 114.,
-			116., 118., 120., 123., 126.,
-			130., 135., 140., 145., 150., 160., 175., 200., 225.,
-			250., 300., 500., 750., 1000.
+			0., 10., 20., 30., 40., 50., 60., 70.,
+			75., 80.,
+			84., 88., 92.,
+			94., 96., 98., 100., 102., 104., 106., 108., 110.,
+			114., 118.,
+			123.,
+			130., 140., 150.,
+			175.,
+			225.,
+			300.,
+			750.,
+			2000.,
+			10000.
 		}
 	);
 
@@ -152,27 +156,24 @@ int main(int argc, char **argv) {
 
 	// Order of events (forward in time)
 	// 1. Borrower: walk-away or repay
-	// 2. Interest payment and (TODO) dividends
-	// 3. Bank: top-up or liquidate
+	// 2. Bank: top-up or liquidate
+	// 3. Interest payment and (TODO) dividends
+
+	// Apply events at all times
+	if(events < 0) { events = N; }
 
 	////////////////////////////////////////////////////////////////////////
 
 	{ // <BorrowerEvent>
 
 	// Time between borrower events
-	const Real dt = T / borrowerEvents;
+	const Real dt = T / events;
 
-	for(int i = 1; i <= borrowerEvents; ++i) {
+	for(int i = 0; i < events; ++i) {
 		const Real t_i = dt * i;
 
 		// Accrued interest
-		const Real A = accruedInterest(t_i - epsilon);
-
-		// TODO: If the borrower event occurs after an interest
-		//       payment, we need to change A(t_i)
-
-		// TODO: Confirm L^{\star}(t+) = L^{\star}(t-)
-		//                             = L^{\star}(t ) ?
+		const Real A = accruedInterest(t_i);
 
 		stepper.add(
 			t_i,
@@ -187,43 +188,16 @@ int main(int argc, char **argv) {
 
 	////////////////////////////////////////////////////////////////////////
 
-	{ // <InterestPayment>
-
-	// Add interest payment events (skip initial time)
-	for(
-		auto it = interestPaymentDates.begin() + 1;
-		it != interestPaymentDates.end();
-		++it
-	) {
-		// Time at which interest payment takes place
-		const Real t_i = *it;
-
-		// Accrued interest
-		const Real A = accruedInterest(t_i);
-
-		stepper.add(
-			t_i,
-			[=] (const Interpolant1 &U, Real S) {
-				return U(S) + L_hat * (A - 1.);
-			},
-			grid
-		);
-	}
-
-	} // </InterestPayment>
-
-	////////////////////////////////////////////////////////////////////////
-
 	{ // <BankEvent>
 
 	// Time between bank events
-	const Real dt = T / bankEvents;
+	const Real dt = T / events;
 
-	for(int i = 1; i <= bankEvents; ++i) {
+	for(int i = 0; i < events; ++i) {
 		const Real t_i = dt * i;
 
 		// Accrued interest
-		const Real A = accruedInterest(t_i + epsilon);
+		const Real A = accruedInterest(t_i);
 
 		stepper.add(
 			t_i,
@@ -232,10 +206,10 @@ int main(int argc, char **argv) {
 				Real best = U(S);
 
 				// Value-to-loan ratio
-				const Real Rinv = S / L_hat;
+				const Real R = L_hat / S;
 
-				// R is below low trigger
-				if(Rinv <= beta_lo) {
+				// R is above high trigger
+				if(R > beta_hi) {
 
 					// Option to liquidate
 					const Real newPos = min(L_hat * A, S);
@@ -244,7 +218,7 @@ int main(int argc, char **argv) {
 					}
 
 				// R is between low and high triggers
-				} else if(Rinv <= beta_hi) {
+				} else if(R >= beta_lo) {
 
 					const Real R_0 = L_0 / S_0;
 
@@ -284,30 +258,58 @@ int main(int argc, char **argv) {
 
 	////////////////////////////////////////////////////////////////////////
 
+	{ // <InterestPayment>
+
+	// Add interest payment events (skip initial time)
+	for(
+		auto it = interestPaymentDates.begin() + 1;
+		it != interestPaymentDates.end();
+		++it
+	) {
+		// Time at which interest payment takes place
+		const Real t_i = *it;
+
+		// Accrued interest
+		const Real A = accruedInterest(t_i);
+
+		stepper.add(
+			t_i,
+			[=] (const Interpolant1 &U, Real S) {
+				return U(S) + L_hat * (A - 1.);
+			},
+			grid
+		);
+	}
+
+	} // </InterestPayment>
+
+	////////////////////////////////////////////////////////////////////////
+
 	// Jump-diffusion operator
 	BlackScholesJumpDiffusion1 bs(
 		grid,
 
 		r,      // Interest
 		sigma,  // Volatility
-		0.,     // Dividend rate
+		0.,     // Continuous dividend rate
 
 		lambda, // Mean arrival time
 		lognormal(mu_xi, sigma_xi) // Log-normal probability density
 	);
 	bs.setIteration(stepper);
 
-	// BDF2
-	ReverseBDFTwo1 bdf2(grid, bs);
-	bdf2.setIteration(stepper);
+	// Discretization method
+	typedef ReverseBDFTwo1 Discretization;
+	Discretization discretization(grid, bs);
+	discretization.setIteration(stepper);
 
 	// Linear system solver
 	BiCGSTABSolver solver;
 	auto U = stepper.solve(
-		grid,   // Domain
-		payoff, // Initial condition
-		bdf2,   // Root of linear system tree
-		solver  // Linear system solver
+		grid,           // Domain
+		payoff,         // Initial condition
+		discretization, // Root of linear system tree
+		solver          // Linear system solver
 	);
 
 	////////////////////////////////////////////////////////////////////////
@@ -315,7 +317,8 @@ int main(int argc, char **argv) {
 	// Similarity reduction
 	const Real alpha = L_hat / L_0;
 	Real value = U(alpha * S_0) / alpha;
-	// 2015-02-28: checked; without events, gives put price with K = L_0
+	// 2015-02-28: checked; without events, exp(-r * T) * L_0 - value is the
+	//                      price (at t=0) of a put with strike L_0
 
 	Real
 		change = value - previousValue,
