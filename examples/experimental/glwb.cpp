914f49361c011f9a158f437f22295f68abc8402d
parsiad <parsiad.azimzadeh@gmail.com>
2017-04-06 17:01:00 -0400

    Added stochastic volatility to optimal consumption problem.

---
+++ examples/experimental/glwb.cpp
@@ -0,0 +1,203 @@
+////////////////////////////////////////////////////////////////////////////////
+// glwb.cpp
+// --------
+//
+// Computes the price of a guaranteed lifelong withdrawal benefits contract as
+// specified in [1]
+//
+// [1] Azimzadeh, Parsiad, and Peter A. Forsyth. "The existence of optimal
+// bang-bang controls for GMxB contracts." SIAM Journal on Financial Mathematics
+// 6.1 (2015): 117-139.
+//
+// Author: Parsiad Azimzadeh
+////////////////////////////////////////////////////////////////////////////////
+
+#include <QuantPDE/Core>
+#include <QuantPDE/Modules/Operators>
+#include <QuantPDE/Modules/Utilities>
+
+using namespace QuantPDE;
+using namespace QuantPDE::Modules;
+
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream> // cerr
+#include <cmath> // abs
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////
+
+Real *R;
+
+class GLWBOperator : public BlackScholes1 {
+
+public:
+
+	GLWBOperator(
+		RectilinearGrid1 &grid,
+		Real r, Real vol, Real management_rate
+	) noexcept : BlackScholes1(grid, r, vol, management_rate) {}
+
+	virtual Vector b(Real t) {
+		Vector S_vec = G.image([] (Real S) { return S; });
+		int n = floor(t);
+		return -(R[n+1] - R[n]) * S_vec;
+	}
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+Real r, vol, S_0, management_rate, withdrawal_rate, penalty_rate;
+int N, T, kn, k0;
+RectilinearGrid1 *grid, *mortality;
+
+// Used to store the optimal control
+Vector optimal_control;
+
+ResultsTuple1 run(int k) {
+
+	// Refinement
+	int factor = 1;
+	for(int i = 0; i < k; ++i) {
+		factor *= 2;
+	}
+	const int N2k = N * factor;
+	auto refined_grid = grid->refined(k);
+
+	SparseLUSolver solver;
+	typedef ReverseBDFTwo Discretization;
+
+	////////////////////////////////////////////////////////////////////////
+
+	GLWBOperator op(refined_grid, r, vol, management_rate);
+
+	ReverseConstantStepper stepper(0., (Real) T, (Real) T / N2k);
+
+	Discretization discretization(refined_grid, op);
+	discretization.setIteration(stepper);
+
+	////////////////////////////////////////////////////////////////////////
+
+	const Real Q = S_0;
+	for(int n = 1; n <= T; ++n) {
+		auto event = [Q, n, k] (const Interpolant1 &V, Real S) {
+			const Real GQ = withdrawal_rate * Q;
+
+			// Nonwithdrawal
+			Real best = V(S);
+			int optimal_control = 0;
+
+			// Withdraw
+			{
+				const Real S_plus = max(S - GQ, 0.);
+				const Real tmp = V(S_plus) + R[n] * GQ;
+				if(tmp > best) {
+					best = tmp;
+					optimal_control = 1;
+				}
+			}
+
+			// Surrender
+			if(S > GQ) {
+				const Real tmp = R[n] * (GQ + (1. - penalty_rate) * (S - GQ));
+				if(tmp > best) {
+					best = tmp;
+					optimal_control = 2;
+				}
+			}
+
+			// Print optimal control
+			if(k == kn && n == 4) {
+				cerr << S << "\t" << optimal_control << endl;
+			}
+
+			return best;
+		};
+		stepper.add((Real) n, event, refined_grid);
+	}
+
+	////////////////////////////////////////////////////////////////////////
+
+	auto solution = stepper.solve(
+		refined_grid,
+		[] (Real) { return 0.; },
+		discretization,
+		solver
+	);
+
+	return ResultsTuple1(
+		{(Real) refined_grid.size(), (Real) N2k },
+		solution, S_0
+	);
+
+}
+
+int main(int argc, char **argv) {
+	// Parse configuration file
+	Configuration configuration = getConfiguration(argc, argv);
+
+	// Get options
+	Real S_max, S_min, dS;
+	kn = getInt(configuration, "maximum_refinement", 8);
+	k0 = getInt(configuration, "minimum_refinement", 0);
+	r = getReal(configuration, "interest_rate", .04);
+	vol = getReal(configuration, "volatility", .2);
+	S_0 = getReal(configuration, "asset_price", 100.);
+	management_rate = getReal(configuration, "management_rate", .015);
+	withdrawal_rate = getReal(configuration, "withdrawal_rate", .05);
+	penalty_rate = getReal(configuration, "penalty_rate", .01);
+	S_min = getReal(configuration, "print_asset_price_minimum", 0.);
+	S_max = getReal(configuration, "print_asset_price_maximum", S_0 * 2.);
+	dS = getReal(configuration, "print_asset_price_step_size", S_0 / 10.);
+	N = getInt(configuration, "initial_number_of_timesteps", 12);
+
+	// PDE grid
+	RectilinearGrid1 default_grid(S_0 * Axis::special);
+	RectilinearGrid1 tmp1 = getGrid(configuration, "initial_grid", default_grid);
+	grid = &tmp1;
+
+	// Mortality data (default is DAV2004R)
+	RectilinearGrid1 default_mortality( Axis {
+		0.008886, 0.009938, 0.011253, 0.012687, 0.014231,
+		0.015887, 0.017663, 0.019598, 0.021698, 0.023990,
+		0.026610, 0.029533, 0.032873, 0.036696, 0.041106,
+		0.046239, 0.052094, 0.058742, 0.066209, 0.074583,
+		0.083899, 0.094103, 0.105171, 0.116929, 0.129206,
+		0.141850, 0.154860, 0.168157, 0.181737, 0.195567,
+		0.209614, 0.223854, 0.238280, 0.252858, 0.267526,
+		0.278816, 0.293701, 0.308850, 0.324261, 0.339936,
+		0.355873, 0.372069, 0.388523, 0.405229, 0.422180,
+		0.439368, 0.456782, 0.474411, 0.492237, 0.510241,
+		0.528401, 0.546689, 0.565074, 0.583517, 0.601976,
+		0.620400, 1.000000
+	} );
+	RectilinearGrid1 tmp2 = getGrid(configuration, "mortality_data", default_mortality);
+	mortality = &tmp2;
+	T = mortality->size();
+
+	// Determine survival rate
+	R = new Real[T+1]; // R[0], ..., R[T]
+	R[0] = 1.;
+	for(int n = 1; n <= T; ++n) {
+		R[n] = R[n-1] * (1. - (*mortality)[0][n]);
+	}
+
+	// Print configuration file
+	cerr << configuration << endl << endl;
+
+	// Run and print results
+	ResultsBuffer1 buffer(
+		run,
+		{ "Nodes", "Steps" },
+		kn, k0
+	);
+	buffer.setPrintGrid( RectilinearGrid1(Axis::range(S_min, dS, S_max)) );
+	buffer.stream();
+
+	// Deallocate
+	delete [] R;
+
+	return 0;
+}
+
--- examples/hjbqvi/optimal_consumption.cpp
+++ examples/hjbqvi/optimal_consumption.cpp
@@ -13,6 +13,11 @@
 // Author: Parsiad Azimzadeh
 ////////////////////////////////////////////////////////////////////////////////
 
+// Uncomment for stochastic volatility
+//#define SV 1
+
+////////////////////////////////////////////////////////////////////////////////
+
 #include <QuantPDE/Core>
 #include <QuantPDE/Modules/HJBQVI>
 
@@ -29,8 +34,13 @@ using namespace std;
 
 int main() {
 
-	// Dimensions
+	#ifdef SV
+	constexpr int Dimension = 3;
+	#else
 	constexpr int Dimension = 2;
+	#endif
+
+	// Dimensions
 	constexpr int StochasticControlDimension = 1;
 	constexpr int ImpulseControlDimension = 1;
 
@@ -76,10 +86,17 @@ int main() {
 	// Solution and control data are printed at this level of refinement
 	const int max_refinement = 6;
 
-	// Used for both dimensions
+	// Used for both account dimensions
 	const Real boundary = 200.;
 	const int spatial_points = 20;
 	const Axis axis = Axis::uniform(0., boundary, spatial_points);
+	const Axis volatility_axis = Axis::uniform(0., 1., spatial_points);
+
+	// Volatility parameters
+	const Real volvol = 0.4;   // Volatility of volatility
+	const Real kappa  = 5.;    // Mean reversion speed
+	const Real theta  = sigma; // Mean
+	const Real v_0    = theta;
 
 	// Gets the value of an impulse control
 	auto z_bounded = [=] (Real t, Real w, Real b, Real z_frac) {
@@ -98,6 +115,12 @@ int main() {
 		return z;
 	};
 
+	#ifdef SV
+	#define ARGS Real w, Real b, Real v
+	#else
+	#define ARGS Real w, Real b
+	#endif
+
 	// Problem description
 	HJBQVI<
 		Dimension,
@@ -108,7 +131,11 @@ int main() {
 		timesteps,
 
 		// Initial spatial grid
+		#ifdef SV
+		{ axis, axis, volatility_axis },
+		#else
 		{ axis, axis },
+		#endif
 
 		// Initial stochastic control grid
 		{ Axis::uniform(0., q_max, control_points) },
@@ -120,54 +147,67 @@ int main() {
 		T,
 
 		// Discount factor
-		[=] (Real t, Real w, Real b) { return rho; },
+		[=] (Real t, ARGS) { return rho; },
 
 		// Volatility
 		{
-			[=] (Real t, Real w, Real b) { return sigma * w; },
-			[=] (Real t, Real w, Real b) { return 0.; }
+			#ifdef SV
+			[=] (Real t, ARGS) { return sqrt(v) * w; },
+			[=] (Real t, ARGS) { return 0.; },
+			[=] (Real t, ARGS) { return volvol * sqrt(v); }
+			#else
+			[=] (Real t, ARGS) { return sigma * w; },
+			[=] (Real t, ARGS) { return 0.; }
+			#endif
 		},
 
 		// Drift
 		{
-			[=] (Real t, Real w, Real b, Real q) { return mu * w; },
-			[=] (Real t, Real w, Real b, Real q) {
+			[=] (Real t, ARGS, Real q) { return mu * w; },
+			[=] (Real t, ARGS, Real q) {
 				// No consumption at boundaries
 				return
 					r * b
 					+ ((0 < b && b < boundary) ? -q : 0.)
 				;
 			}
+			#ifdef SV
+			, [=] (Real t, ARGS, Real q) {
+				return kappa * (theta - v);
+			}
+			#endif
 		},
 
 		// Continuous cash/utility/etc. flow
-		[=] (Real t, Real w, Real b, Real q) {
+		[=] (Real t, ARGS, Real q) {
 			// No consumption at boundaries
 			return (0 < b && b < boundary) ? pow(q, gamma)/gamma:0.;
 		},
 
 		// Impulse transition
 		{
-			[=] (Real t, Real w, Real b, Real z_frac) {
+			[=] (Real t, ARGS, Real z_frac) {
 				const Real z = z_bounded(t,w,b,z_frac);
 				return w + z;
 			},
-			[=] (Real t, Real w, Real b, Real z_frac) {
+			[=] (Real t, ARGS, Real z_frac) {
 				const Real z = z_bounded(t,w,b,z_frac);
 				return b - z - lambda * fabs(z) - c;
 			}
+			#ifdef SV
+			, [=] (Real t, ARGS, Real z_frac) { return v; }
+			#endif
 		},
 
 		// Impulse cash/utility/etc. reward
-		[=] (Real t, Real w, Real b, Real z_frac) {
+		[=] (Real t, ARGS, Real z_frac) {
 			// Impulse
 			const Real z = z_bounded(t,w,b,z_frac);
-			if(z==0.) { return -numeric_limits<Real>::infinity(); }
-			return 0.;
+			return z == 0. ? -numeric_limits<Real>::infinity() : 0.;
 		},
 
 		// Cash/utility/etc. reward at expiry
-		[=] (Real t, Real w, Real b) {
+		[=] (Real t, ARGS) {
 			const Real liquidate = max(b + (1-lambda) * w - c, 0.);
 			return pow(liquidate, gamma) / gamma;
 		}
@@ -187,7 +227,14 @@ int main() {
 	// Run
 	HJBQVI_main(
 		hjbqvi,
-		{ w_0, b_0 }, // Convergence test at (w=w_0, b=b_0)
+
+		// Convergence test at (w=w_0, b=b_0, v=v_0)
+		#ifdef SV
+		{ w_0, b_0, v_0 },
+		#else
+		{ w_0, b_0 },
+		#endif
+
 		max_refinement
 	);
 
